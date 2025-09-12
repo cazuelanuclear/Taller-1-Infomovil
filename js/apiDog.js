630e76624b2b5f6c7e15e630c3dbbbb33172cfe5
Benjamín Torres <168466934+cazuelanuclear@users.noreply.github.com>
2025-09-11 23:16:34 -0300

    cambios en el boton
    ahora la api se abre en otro html con el botón. tomenlo como plantilla para las demás

--- js/apiDog.js
+++ js/apiDog.js
@@ -1,18 +1,20 @@
-const fetchButton = document.getElementById("fetchApiDog");
 const dataContainer = document.getElementById("dataContainer")
 
 async function fetchData() {
     const res = await fetch("https://api.thedogapi.com/v1/breeds");
     const data = await res.json();
-    dataContainer.innerHTML = '<h2>Resultados de la data:</h2>';
+    dataContainer.innerHTML = '<h1>Razas de los perritos:</h2>';
 
     data.forEach(dog => {
         dataContainer.innerHTML +=`
-            <div class="p-4 border-b border-gray-200">
+            <div class="p-4 border border-gray-200 bg-blue-50">
                 <h3 class="text-xl font-bold">${dog.name}</h3>
                 <p><strong>ID:</strong> ${dog.id}</p>
+                <p><strong>Origen:</strong> ${dog.origin || 'Desconocido'}</p>
+                <p><strong>Peso:</strong> ${dog.weight.imperial} lbs / ${dog.weight.metric} kg</p>
+                <p><strong>Altura:</strong> ${dog.height.imperial} in / ${dog.height.metric} cm</p>
             </div>`;
     });
 }
 
-fetchButton.addEventListener('click', fetchData)
+fetchData();
---
+++ js/indexScript.js
@@ -0,0 +1,4 @@
+const fetchButton = document.getElementById("fetchApiDog").addEventListener("click", () => {
+      
+      window.location.href = "dog_api.html";
+});;
