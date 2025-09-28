b95229208e2391f35bca56b5cdc0c87754bbfeaa
M-urrutia <murru1605@gmail.com>
2025-09-28 12:32:54 -0300

    Add files via upload

--- js/apiCat.js
+++ js/apiCat.js
@@ -1,30 +1,51 @@
-const dataContainer = document.getElementById("dataContainer")
+const dataContainer = document.getElementById("dataContainer");
+var numPaginaActual = 1;
+const paginaActual = document.getElementById("paginaActual");
+const paginaAnterior = document.getElementById("paginaAnterior");
+const paginaSiguiente = document.getElementById("paginaSiguiente");
+const contenedorBotones = document.getElementById("contenedorBotones");
 
 async function fetchData() {
-    var res = await fetch("https://catfact.ninja/facts");
+    var res = await fetch("https://catfact.ninja/facts?page="+numPaginaActual);
     var data = await res.json();
+	dataContainer.innerHTML = "";
 
     data.data.forEach(cat => {
         dataContainer.innerHTML +=`
-            <div class="p-4 border border-gray-200 bg-blue-50">
+            <div class="p-4 border border-gray-200 bg-blue-50 rounded-lg shadow-lg">
                 <h3 class="text-xl font-bold">Fact:</h3>
-                <p><strong>ID:</strong> ${cat.fact}</p>
+                <p>${cat.fact}</p>
             </div>`;
     });
-
-
-    res = await fetch("https://catfact.ninja/facts?page=2");
-    data = await res.json();
-
-    data.data.forEach(cat => {
-        dataContainer.innerHTML +=`
-            <div class="p-4 border border-gray-200 bg-blue-50">
-                <h3 class="text-xl font-bold">Fact:</h3>
-                <p><strong>ID:</strong> ${cat.fact}</p>
-            </div>`;
-    });
-
-
 }
+
+function mostarBotones(){
+	paginaAnterior.innerText = numPaginaActual - 1;
+	paginaActual.innerText = numPaginaActual;
+	paginaSiguiente.innerText = numPaginaActual + 1;
+	
+	if (numPaginaActual == 1){
+		paginaAnterior.classList.add("hidden");
+		paginaSiguiente.classList.remove("hidden");
+	} else if (numPaginaActual == 34){
+		paginaAnterior.classList.remove("hidden");
+		paginaSiguiente.classList.add("hidden");
+	} else {
+		paginaAnterior.classList.remove("hidden");
+		paginaSiguiente.classList.remove("hidden");
+	}
+}
+
+paginaSiguiente.addEventListener("click", () => {
+    numPaginaActual += 1;
+    fetchData();
+    mostarBotones();
+});
+
+paginaAnterior.addEventListener("click", () => {
+    numPaginaActual -= 1;
+    fetchData();
+    mostarBotones();
+});
 
 fetchData();
