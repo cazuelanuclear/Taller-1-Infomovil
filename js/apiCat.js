7df53349a8689e4fdd352ce075e7fa272347fe6f
AndresGHR2015 <andres.hidalgo01@alumnos.ucn.cl>
2025-09-28 14:03:57 -0300

    Added random fact button functionality

--- js/apiCat.js
+++ js/apiCat.js
@@ -4,12 +4,12 @@ const paginaActual = document.getElementById("paginaActual");
 const paginaAnterior = document.getElementById("paginaAnterior");
 const paginaSiguiente = document.getElementById("paginaSiguiente");
 const contenedorBotones = document.getElementById("contenedorBotones");
+const randomBtn = document.getElementById("randomBtn");
 
 async function fetchData() {
     var res = await fetch("https://catfact.ninja/facts?page="+numPaginaActual);
     var data = await res.json();
 	dataContainer.innerHTML = "";
-
     data.data.forEach(cat => {
         dataContainer.innerHTML +=`
             <div class="p-4 border border-gray-200 bg-blue-50 rounded-lg shadow-lg">
@@ -35,6 +35,20 @@ function mostarBotones(){
 		paginaSiguiente.classList.remove("hidden");
 	}
 }
+
+async function showRandomFact() {
+    const res = await fetch("https://catfact.ninja/fact");
+    const data = await res.json();
+
+    dataContainer.innerHTML = `
+      <div class="p-4 border border-gray-200 bg-yellow-100 rounded-lg shadow-lg">
+        <h3 class="text-xl font-bold mb-2">Random Fact:</h3>
+        <p class="text-gray-700">${data.fact}</p>
+      </div>
+    `;
+}
+
+randomBtn.addEventListener('click', showRandomFact);
 
 paginaSiguiente.addEventListener("click", () => {
     numPaginaActual += 1;
