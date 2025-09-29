a5d22cd959430f25140bd1eb4002abbcf99c3c60
Benjamín Torres <168466934+cazuelanuclear@users.noreply.github.com>
2025-09-28 23:03:19 -0300

    ultimo commit

--- js/apiCat.js
+++ js/apiCat.js
@@ -39,7 +39,8 @@ function mostarBotones(){
 async function showRandomFact() {
     const res = await fetch("https://catfact.ninja/fact");
     const data = await res.json();
-
+    dataContainer.classList.remove('sm:grid-cols-2', 'lg:grid-cols-3');
+    dataContainer.classList.add('grid-cols-1');
     dataContainer.innerHTML = `
       <div class="p-4 border border-gray-200 bg-yellow-100 rounded-lg shadow-lg">
         <h3 class="text-xl font-bold mb-2">Random Fact:</h3>
@@ -52,12 +53,14 @@ randomBtn.addEventListener('click', showRandomFact);
 
 paginaSiguiente.addEventListener("click", () => {
     numPaginaActual += 1;
+    dataContainer.classList.add('sm:grid-cols-2', 'lg:grid-cols-3');
     fetchData();
     mostarBotones();
 });
 
 paginaAnterior.addEventListener("click", () => {
     numPaginaActual -= 1;
+    dataContainer.classList.add('sm:grid-cols-2', 'lg:grid-cols-3');
     fetchData();
     mostarBotones();
 });
